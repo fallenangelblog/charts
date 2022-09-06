a9e9dff7eb8edcb1975252d0ba3bf8ac179e429b
陈燚 <chenyi@hzyele.com>
2022-09-06 16:42:04 +0800

    rebuild

--- repackage.sh
+++ repackage.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
 
-find . --type
+set -x
+set -e
+
+find .  -maxdepth 1 -mindepth 1  -not -path '*/\.*' -type d -exec helm package {} \;
+helm repo index --url https://fallenangelblog.github.io/charts/ .
+
+
+git add . && git commit -m 'rebuild' && git push
